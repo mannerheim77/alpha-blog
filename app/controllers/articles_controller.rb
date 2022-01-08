5c105cc8aa0eb43ae30611bdb266b2e24f407f71
mannerheim77 <bellamyc@sbcglobal.net>
2022-01-08 10:57:22 -0600

    Creating an Article from the Browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+
+    # will result in a ForbiddenAttributesError.  Because you need to whitelist what's coming in from the web
+    # @article = Article.new(params[:article])
+
+    #require top-level key of article and permit title and description to be used to create article object
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    @article.save
+
+    # redirect_to article_path(@article)
+    # redirect_to @article  #a shortcut for the previous statement
+
+    redirect_to articles_path
+
+    # render plain: @article.inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   #resources :articles  #shortcut way to give you all CRUD routes for articles
   #resources :articles, only: [:show]  #only exposes the GET
-  resources :articles, only: [:show, :index]
+  # resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
 end
