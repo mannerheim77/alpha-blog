e9681eb3cda07d6474d4d9f1f1ef044a6af75945
mannerheim77 <bellamyc@sbcglobal.net>
2022-01-08 09:39:24 -0600

    Articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,8 @@ class ArticlesController < ApplicationController
     # byebug  # pause here
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   #resources :articles  #shortcut way to give you all CRUD routes for articles
-  resources :articles, only: [:show]  #only exposes the GET
+  #resources :articles, only: [:show]  #only exposes the GET
+  resources :articles, only: [:show, :index]
+
 end
